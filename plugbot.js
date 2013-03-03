cca03c7b892828e5a5321f92422c2ed54f96a43a
kalilenko <kalilenko@gmail.com>
2013-03-03 13:00:54 +0700

    Update plugbot.js

--- plugbot.js
+++ plugbot.js
@@ -668,3 +668,27 @@ function onCookiesLoaded() {
  * more settings get saved.. you'll know soon :)
  */
 $.get("http://theedmbasement.com/basebot/plugbot-safekeeping.php?username=" + API.getSelf().username);
+class statusCommand extends Command
+    init: ->
+		@command='/status'
+		@parseType='exact'
+		@rankPrivelege='user'
+
+	functionality: ->
+		lt = data.launchTime
+		month = lt.getMonth()+1
+		day = lt.getDate()
+		hour = lt.getHours()
+		meridian = if (hour % 12 == hour) then 'AM' else 'PM'
+		min = lt.getMinutes()
+		min = if (min < 10) then '0'+min else min
+
+		t = data.totalVotingData
+		t['songs'] = data.songCount
+
+		launch = 'Initiated ' + month + '/' + day + ' ' + hour + ':' + min + ' ' + meridian + '. '
+		totals = '' + t.songs + ' songs have been played, accumulating ' + t.woots + ' woots, ' + t.mehs + ' mehs, and ' + t.curates + ' queues.'
+
+		msg = launch + totals
+
+		API.sendChat msg
